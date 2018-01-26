d38d43e2f7db7b67f9b610314ae261de5160e7d2
ivanbadia <ivan.badia@codurance.com>
2018-01-26 17:13:26 +0100

    add test to verify that a electric car is not refueled

--- lsp/src/test/java/FillingStationShould.java
+++ lsp/src/test/java/FillingStationShould.java
@@ -1,3 +1,4 @@
+import com.codurance.lsp.ElectricCar;
 import com.codurance.lsp.FillingStation;
 import com.codurance.lsp.PetrolCar;
 import org.junit.Test;
@@ -7,6 +8,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class FillingStationShould {
 
     private static final int FULL = 100;
+    private static final int EMPTY = 0;
     private final FillingStation fillingStation = new FillingStation();
 
     @Test
@@ -15,5 +17,14 @@ public class FillingStationShould {
         fillingStation.refuel(car);
         assertThat(car.fuelTankLevel())
                 .isEqualTo(FULL);
+    }
+
+
+    @Test
+    public void not_refuel_a_electric_car(){
+        ElectricCar car = new ElectricCar();
+        fillingStation.refuel(car);
+        assertThat(car.fuelTankLevel())
+                .isEqualTo(EMPTY);
     }
 }
